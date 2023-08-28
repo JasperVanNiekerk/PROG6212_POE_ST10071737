189556f2f2e2d26f1e433088366cc88159d47a37
Try-Angle1134 <102529964+ST10071737@users.noreply.github.com>
2023-08-28 17:57:10 +0200

    added som more view  and basic logic

---
+++ PROG6212_POE_ST10071737/MVVM/View/BigCore.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PROG6212_POE_ST10071737.MVVM.View
+{
+    /// <summary>
+    /// Interaction logic for BigCore.xaml
+    /// </summary>
+    public partial class BigCore : UserControl
+    {
+        //___________________________________________________________________________________________________________
+        //__________________________________________Constructors_____________________________________________________
+        //___________________________________________________________________________________________________________
+
+        /// <summary>
+        /// constructor used to set the display for the big core
+        /// </summary>
+        public BigCore()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            Storyboard animation1 = (Storyboard)FindResource("Arc1animation");
+            animation1.Begin(Arc1Border);
+
+            Storyboard animation2 = (Storyboard)FindResource("Arc2animation");
+            animation2.Begin(Arc2Border);
+
+            Storyboard animation3 = (Storyboard)FindResource("Arc3animation");
+            animation3.Begin(Arc3Border);
+
+            Storyboard animation4 = (Storyboard)FindResource("BrokenCircleAnimation");
+            animation4.Begin(BrokenCircleBorder);
+        }
+    }
+}
---
+++ PROG6212_POE_ST10071737/MVVM/ViewModel/LoginViewModel.cs
@@ -0,0 +1,29 @@
+﻿using PROG6212_POE_ST10071737.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PROG6212_POE_ST10071737.MVVM.ViewModel
+{
+    internal class LoginViewModel
+    {
+        private RelayCommand LoginCommand { get; set; }
+
+        public MainWindowViewModel MainWindowViewModel { get; set; }
+
+        public LoginViewModel() 
+        {
+            LoginCommand = new RelayCommand(this.LoginUser);
+        }
+
+        private void LoginUser(object p)
+        {
+            var MainWindow = new MainWindow();
+            MainWindow.Show();
+
+        }
+    }
+}
